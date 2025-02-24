5eda55d6e0a2ee02515a87f6593e30235bfed86c
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2025-02-24 12:02:52 +0100

    Update login.js

--- backend/src/main/resources/static/js/login.js
+++ backend/src/main/resources/static/js/login.js
@@ -1,24 +1,24 @@
-// // Show success or error messages using SweetAlert2
-// document.addEventListener("DOMContentLoaded", function() {
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const message = urlParams.get('message');
-//     const error = urlParams.get('error');
-//
-//     if (message) {
-//         Swal.fire({
-//             icon: 'success',
-//             title: 'Success',
-//             text: message,
-//             confirmButtonText: 'OK'
-//         });
-//     }
-//
-//     if (error) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Error',
-//             text: error,
-//             confirmButtonText: 'OK'
-//         });
-//     }
-// });
+// Show success or error messages using SweetAlert2
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const message = urlParams.get('message');
+    const error = urlParams.get('error');
+
+    if (message) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: message,
+            confirmButtonText: 'OK'
+        });
+    }
+
+    if (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error,
+            confirmButtonText: 'OK'
+        });
+    }
+});
