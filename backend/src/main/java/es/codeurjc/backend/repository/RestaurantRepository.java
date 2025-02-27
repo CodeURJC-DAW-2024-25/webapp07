d23a2c7f12893ae0ca1d7b97e829093b244b69cc
Victor-munoz78 <v.munozr.2020@alumnos.urjc.es>
2025-02-27 21:24:41 +0100

    Update: RestaurantRepository

--- backend/src/main/java/es/codeurjc/backend/repository/RestaurantRepository.java
+++ backend/src/main/java/es/codeurjc/backend/repository/RestaurantRepository.java
@@ -1,4 +1,10 @@
 package es.codeurjc.backend.repository;
 
-public interface RestaurantRepository {
+import es.codeurjc.backend.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    // Métodos adicionales si es necesario en el futuro
 }
