a3a3a5b3f8cf256ede12c7bbd68c7ab7f8e96aff
hugo-str <hugost03@gmail.com>
2025-03-18 10:06:03 +0100

    Update: DishDTO

--- backend/src/main/java/es/codeurjc/backend/dto/DishDTO.java
+++ backend/src/main/java/es/codeurjc/backend/dto/DishDTO.java
@@ -5,18 +5,18 @@ import es.codeurjc.backend.enums.Allergens;
 import java.sql.Blob;
 import java.util.List;
 
-public class DishDTO {
-    private Long id;
-    private String name;
-    private String description;
-    private int price;
-    private List<String> ingredients;
-    private List<Allergens> allergens;
-    private boolean isVegan;
-    private Blob dishImagefile;
-    private boolean image;
-    private String dishImagePath;
-    private boolean isAvailable;
-    private List<Integer> rates;
-    private int rate;
-}
+public record DishDTO(
+    Long id,
+    String name,
+    String description,
+    int pric,
+    List<String> ingredients,
+    List<Allergens> allergens,
+    boolean isVegan,
+    Blob dishImagefile,
+    boolean image,
+    String dishImagePat,
+    boolean isAvailable,
+    List<Integer> rates,
+    int rate
+){};
