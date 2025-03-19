ba5790ee1c51a72cf61f22cc61fe7c30c57b3c47
hugo-str <hugost03@gmail.com>
2025-03-19 19:00:37 +0100

    Update: UserDTO

--- backend/src/main/java/es/codeurjc/backend/dto/UserDTO.java
+++ backend/src/main/java/es/codeurjc/backend/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package es.codeurjc.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -17,5 +19,6 @@ public record UserDTO(
         String address,
         String email,
         List<String> roles,
-        boolean banned
+        boolean banned,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) String password
 ) {}
