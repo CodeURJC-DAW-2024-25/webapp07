c735c538591f16f770723fc09d51f2806a714fdb
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2025-04-04 18:49:41 +0200

    add header and footer duplicated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,20 @@ import {AppComponent} from "./components/app.component";
 import {HttpClientModule} from "@angular/common/http";
 import {FooterComponent} from "./components/generalComponents/footer/footer.component";
 import {BrowserModule} from "@angular/platform-browser";
+import {HeaderComponent} from "./components/generalComponents/header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule
   ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
