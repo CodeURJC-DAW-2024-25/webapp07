4dfa8cb103f61ef48a3e91ad436c7382e61d0ee4
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2025-04-22 10:29:01 +0200

    Create SPAController.java

---
+++ backend/src/main/java/es/codeurjc/backend/controller/SPAController.java
@@ -0,0 +1,12 @@
+package es.codeurjc.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+    @GetMapping({ "/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}","/new/" })
+    public String redirect() {
+        return "forward:/new/index.html";
+    }
+}
