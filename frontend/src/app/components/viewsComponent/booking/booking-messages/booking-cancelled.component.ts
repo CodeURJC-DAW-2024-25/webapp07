ff8a2da6a0396fef0ae6270dc4be4ef5b8aff6b7
Victor-munoz78 <v.munozr.2020@alumnos.urjc.es>
2025-04-25 03:44:35 +0200

    Add: Messages Screens

---
+++ frontend/src/app/components/viewsComponent/booking/booking-messages/booking-cancelled.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-cancelled',
+  templateUrl: './booking-cancelled.component.html',
+  styleUrls: ['./booking-messages.component.css']
+})
+export class BookingCancelledComponent {}
---
+++ frontend/src/app/components/viewsComponent/booking/booking-messages/booking-confirmation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-confirmation',
+  templateUrl: './booking-confirmation.component.html',
+  styleUrls: ['./booking-messages.component.css']
+})
+export class BookingConfirmationComponent {}
