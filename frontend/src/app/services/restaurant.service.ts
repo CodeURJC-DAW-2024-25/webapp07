59411956210a449035217d686190656d42d8f52c
jesuslpzZz <102820470+jesuslpzZz@users.noreply.github.com>
2025-04-27 19:39:38 +0200

    Fix: routes

--- frontend/src/app/services/restaurant.service.ts
+++ frontend/src/app/services/restaurant.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Restaurant } from '../dtos/restaurant.model'; // Asegúrate de definir el modelo en el archivo 'restaurant.model.ts'
+import { Restaurant } from '../dtos/restaurant.model';
+import {Booking} from "../dtos/booking.model"; // Asegúrate de definir el modelo en el archivo 'restaurant.model.ts'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
-  private apiUrl = 'api/v1/restaurants';
+  private apiUrl = `api/v1/restaurants`;
 
   constructor(private http: HttpClient) {}
 
@@ -21,8 +22,7 @@ export class RestaurantService {
   }
 
   searchByLocation(query: string): Observable<Restaurant[]> {
-    const params = new HttpParams().set('location', query);
-    return this.http.get<Restaurant[]>(this.apiUrl, { params });
+    return this.http.get<Restaurant[]>(`${this.apiUrl}?query=${query}`);
   }
 
   createRestaurant(restaurant: Restaurant): Observable<Restaurant> {
