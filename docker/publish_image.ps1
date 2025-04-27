ebb3582bea84cb218c1835f9720d95ad86b2d207
hugo-str <hugost03@gmail.com>
2025-04-27 21:48:00 +0200

    Update: readme

--- docker/publish_image.ps1
+++ docker/publish_image.ps1
@@ -1,8 +1,6 @@
 # Variables
 $dockerHubUser = "hugost010"  # <-- CAMBIA esto a tu usuario real
-$backendImage = "$dockerHubUser/volteretacroqueta-backend:latest"
-$frontendImage = "$dockerHubUser/volteretacroqueta-frontend:latest"
+$image = "$dockerHubUser/volteretacroqueta:latest"
 
 # Pushear imágenes a Docker Hub
-docker push $backendImage
-docker push $frontendImage
+docker push $image
